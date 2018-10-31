51a62bb3ffd30560b1157f77ba8ba4d70e8418b7
Warren Ferrell <warren.ferrell@colorado.edu>
2018-10-30 23:41:57 -0600

    Converted Interpreters to use a Template Design pattern.

--- src/shared/CodeAFriend.Languages.Python2/Python2Interpreter.cs
+++ src/shared/CodeAFriend.Languages.Python2/Python2Interpreter.cs
@@ -10,39 +10,38 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Threading;
 using System.Reflection;
+using CodeAFriend.Languages.Core;
 
 namespace CodeAFriend.Languages.Python2
 {
 	/// <summary>
 	/// 
 	/// </summary>
-	public class Python2Interpreter : ILanguageInterpreter
+	public class Python2Interpreter : InterpreterTemplate
 	{
-		public string Name { get; } = nameof(Python);
+		public override string Name { get; } = nameof(Python2Interpreter);
 
-		/// <inheritdoc/>
-		public async Task<ScriptEvaluation> ExecuteAsync(RuntimeParameters parameters)
+		/// <remarks>from 
+		/// https://medium.com/emoney-engineering/running-python-script-from-c-and-working-with-the-results-843e68d230e5 .
+		/// https://kimsereyblog.blogspot.com/2018/01/start-processes-from-c-in-dotnet-core.html 
+		/// </remarks>
+		public override ProcessStartInfo GetProcessStartInfo(string scriptFilePath)
 		{
-			return await RunProcessAsync(parameters);
-		}
+			var thisAssembly = typeof(Python2Interpreter).Assembly;
+			var arguments = $"{thisAssembly.ManifestModule.Name} {scriptFilePath}";
 
+			return new ProcessStartInfo("dotnet", arguments);
+		}
 
 		public static async Task Main(string[] args)
 		{
-			string scriptBody = Encoding.UTF8.GetString(Convert.FromBase64String(args[0]));
-			Console.Write(await RunEngineAsync(scriptBody));
+			string scriptFilePath = args[0];
+			Console.Write(await RunEngineAsync(await File.ReadAllTextAsync(scriptFilePath)));
 
-			Thread.Sleep( 555555555);
 			return;
 		}
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="parameter"></param>
-		/// <param name="serviceid"></param>
 		/// <remarks>from https://medium.com/emoney-engineering/running-python-script-from-c-and-working-with-the-results-843e68d230e5 </remarks>
-		/// <returns></returns>
 		public static async Task<string> RunEngineAsync(string scriptBody)
 		{
 			ScriptEngine engine = IronPython.Hosting.Python.CreateEngine(); // Extract Python language engine from their grasp
@@ -64,101 +63,7 @@ namespace CodeAFriend.Languages.Python2
 			}
 		}
 
-		public static string AssemblyDirectory
-		{
-			get
-			{
-				string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-				UriBuilder uri = new UriBuilder(codeBase);
-				string path = Uri.UnescapeDataString(uri.Path);
-				return Path.GetDirectoryName(path);
-			}
-		}
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="parameter"></param>
-		/// <param name="serviceid"></param>
-		/// <remarks>from https://medium.com/emoney-engineering/running-python-script-from-c-and-working-with-the-results-843e68d230e5 .
-		/// https://kimsereyblog.blogspot.com/2018/01/start-processes-from-c-in-dotnet-core.html </remarks>
-		/// <returns></returns>
-		private async Task<ScriptEvaluation> RunProcessAsync(RuntimeParameters parameters)
-		{
-			var thisAssembly = typeof(Python2Interpreter).Assembly;
-			var arguments = $"{thisAssembly.ManifestModule.Name} {Convert.ToBase64String(Encoding.UTF8.GetBytes(parameters.ScriptBody))}";
 
-			ProcessStartInfo startInfo = new ProcessStartInfo("dotnet", arguments);
-			startInfo.CreateNoWindow = true;
-			startInfo.UseShellExecute = false;
-			startInfo.FileName = "dotnet";
-			startInfo.Arguments = arguments;
-			startInfo.RedirectStandardOutput = true;
-			startInfo.RedirectStandardInput = true;
-			startInfo.RedirectStandardError = true;
-
-			var currentdir = Directory.GetCurrentDirectory();
-
-			Directory.SetCurrentDirectory(AssemblyDirectory);
-			var executingDir = AssemblyDirectory;
-			var executingLoc = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-			using (var process = Process.Start(startInfo))
-			{
-				Debug.WriteLine(process.ProcessName);
-				Debug.WriteLine(process.Id);
-				string error = await process.StandardError.ReadToEndAsync();
-				if (!string.IsNullOrWhiteSpace(error))
-				{
-					throw new Exception(error);
-				}
-				//process.StandardOutput.BaseStream.Seek(0, SeekOrigin.Begin);
-				using (StreamReader reader = process.StandardOutput)
-				{
-					
-					//reader.BaseStream.Seek(0, SeekOrigin.Begin);
-					string result = await reader.ReadToEndAsync();
-					return new ScriptEvaluation(
-						output: result,
-						cpuTime: process.UserProcessorTime.TotalMilliseconds,
-						memoryUsage: process.PeakPagedMemorySize64 + process.PeakVirtualMemorySize64,
-						parameters: parameters
-				);
-				}
-
-					//process.Inpu
-
-					//process.OutputDataReceived += (sender, data) => {
-					//	Console.WriteLine(data.Data);
-					//};
-
-					////var inputStream = new MemoryStream(Encoding.UTF8.GetBytes(parameters.Input));
-					//process.S
-					process.Start();
-				
-				await process.StandardInput.WriteAsync(parameters.Input.AsMemory());
-
-				CancellationTokenSource source = new CancellationTokenSource();
-				Task processTask = new Task(async () => await Task.Delay((int) 1000, source.Token));
-				process.Exited += (sender, data) => {
-					source.Cancel();
-				};
-				await processTask;
-				process.WaitForExit(1000);
-				process.StandardOutput.BaseStream.Seek(0, SeekOrigin.Begin);  
-				return new ScriptEvaluation(
-					output: await process.StandardOutput.ReadToEndAsync(),
-					cpuTime: process.UserProcessorTime.TotalMilliseconds,
-					memoryUsage: process.PeakPagedMemorySize64 + process.PeakVirtualMemorySize64,
-					parameters: parameters
-				);
-			}
-			
-		}
-
-		private void Process_Exited(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
--- src/shared/CodeAFriend.Languages.Python3/Python3Interpreter.cs
+++ src/shared/CodeAFriend.Languages.Python3/Python3Interpreter.cs
@@ -12,97 +12,23 @@ using System.Threading;
 using System.Reflection;
 using CodeAFriend.DataModel.Constants;
 using System.Text.RegularExpressions;
+using CodeAFriend.Languages.Core;
 
 namespace CodeAFriend.Languages.Python3
 {
 	/// <summary>
 	/// 
 	/// </summary>
-	public class Python3Interpreter : ILanguageInterpreter
+	public class Python3Interpreter : InterpreterTemplate
 	{
-		public string Name { get; } = nameof(Python);
+		public override string Name { get; } = nameof(Python3Interpreter);
 
-		/// <inheritdoc/>
-		public async Task<ScriptEvaluation> ExecuteAsync(RuntimeParameters parameters)
-		{
-			return await RunProcessAsync(parameters);
-		}
-
-
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="parameter"></param>
-		/// <param name="serviceid"></param>
 		/// <remarks>run to create an alias  
 		/// doskey python3="C:\Program Files\Python37\python.exe"
 		/// </remarks>
-		/// <returns></returns>
-		private async Task<ScriptEvaluation> RunProcessAsync(RuntimeParameters parameters)
+		public override ProcessStartInfo GetProcessStartInfo(string scriptFilePath)
 		{
-			Directory.CreateDirectory(DirectoryPaths.Scripts);
-			var scriptFilePath = $"{DirectoryPaths.Scripts}/{Guid.NewGuid()}.py3";
-			var scriptFile = File.OpenWrite(scriptFilePath);
-			await scriptFile.WriteAsync(Encoding.UTF8.GetBytes(parameters.ScriptBody));
-			scriptFile.Close();
-
-			ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Program Files\Python37\python.exe", scriptFilePath); // todo: make this referenced by an alias
-			startInfo.CreateNoWindow = true;
-			startInfo.UseShellExecute = false;
-			startInfo.RedirectStandardOutput = true;
-			startInfo.RedirectStandardInput = true;
-			startInfo.RedirectStandardError = true;
-
-			using (var process = Process.Start(startInfo))
-			{
-				using (StreamWriter input = process.StandardInput)
-				{
-					await input.WriteAsync(parameters.Input.AsMemory());
-				}
-
-
-				Debug.WriteLine(process.ProcessName);
-				Debug.WriteLine(process.Id);
-				string error = await process.StandardError.ReadToEndAsync();
-				if (!string.IsNullOrWhiteSpace(error))
-				{
-					throw new Exception(error);
-				}
-				using (StreamReader reader = process.StandardOutput)
-				{
-					
-					string result = await reader.ReadToEndAsync();
-					return new ScriptEvaluation(
-						output: result,
-						cpuTime: process.UserProcessorTime.TotalMilliseconds,
-						memoryUsage: process.PeakPagedMemorySize64 + process.PeakVirtualMemorySize64,
-						parameters: parameters
-				);
-				}
-
-
-				CancellationTokenSource source = new CancellationTokenSource();
-				Task processTask = new Task(async () => await Task.Delay((int) 1000, source.Token));
-				process.Exited += (sender, data) => {
-					source.Cancel();
-				};
-				await processTask;
-				process.WaitForExit(1000);
-				process.StandardOutput.BaseStream.Seek(0, SeekOrigin.Begin);  
-				return new ScriptEvaluation(
-					output: await process.StandardOutput.ReadToEndAsync(),
-					cpuTime: process.UserProcessorTime.TotalMilliseconds,
-					memoryUsage: process.PeakPagedMemorySize64 + process.PeakVirtualMemorySize64,
-					parameters: parameters
-				);
-			}
-			
-		}
-
-		private void Process_Exited(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
+			return new ProcessStartInfo(@"C:\Program Files\Python37\python.exe", scriptFilePath);
 		}
 	}
 }
