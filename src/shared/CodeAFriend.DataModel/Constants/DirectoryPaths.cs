d3ae6a744a71588b139ea1882fef5e5d91d32436
Warren Ferrell <warren.ferrell@colorado.edu>
2018-10-30 20:52:55 -0600

    Made python3 interpreter library

---
+++ src/shared/CodeAFriend.DataModel/Constants/DirectoryPaths.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeAFriend.DataModel.Constants
+{
+	public static class DirectoryPaths
+	{
+		public const string Scripts = "./scripts";
+	}
+}
